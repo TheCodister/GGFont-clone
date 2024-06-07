a5b6c3afae52585eba5aabbf9bf9969c373d1f29
TheCodister <quang.nguyen26012907@hcmut.edu.vn>
2024-06-07 08:47:15 +0700

    fix: add SWR fixed

--- src/api/services/getFont.tsx
+++ src/api/services/getFont.tsx
@@ -1,10 +1,11 @@
+"use client";
 import axios from "axios";
 import API_KEY from "../api_key";
 import useSWR from "swr";
-
+// const fetcher = (...args) => fetch(...args).then((res) => res.json());
 const fetcher = async () => {
   const res = await axios.get(
-    `https://www.googleapis.com/webfonts/v1/webfonts?key=${API_KEY}`
+    "https://www.googleapis.com/webfonts/v1/webfonts?key=" + API_KEY
   );
   return res;
 };
@@ -30,17 +31,29 @@ const GetFonts = () => {
 //     }));
 // };
 
-const getFontVariantFile = async (fileUrl: string) => {
-  return axios
-    .get(fileUrl, { responseType: "blob" })
-    .then((result) => ({
-      status: "ok",
-      data: result.data,
-    }))
-    .catch((error) => ({
-      status: "error",
-      error: error.data,
-    }));
+// const getFontVariantFile = async (fileUrl: string) => {
+//   return axios
+//     .get(fileUrl, { responseType: "blob" })
+//     .then((result) => ({
+//       status: "ok",
+//       data: result.data,
+//     }))
+//     .catch((error) => ({
+//       status: "error",
+//       error: error.data,
+//     }));
+// };
+const fetchFontVariant = async (fileUrl: string) => {
+  const res = await axios.get(fileUrl, { responseType: "blob" });
+  return res;
+};
+const GetFontVariantFile = async (fileUrl: string) => {
+  const { data, error } = useSWR(fileUrl, fetchFontVariant);
+  return {
+    data: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
 };
 
-export { GetFonts, getFontVariantFile };
+export { GetFonts, GetFontVariantFile };
